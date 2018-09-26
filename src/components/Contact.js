be62e00f3243bcc9301b8a7de6be380dfd37f02f
Aldo Monteiro <aldomonteiro@gmail.com>
2018-09-25 22:24:14 -0300

    form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const Contact = (props) => (
         <div className="inner">
 
             {/* <form name="contact" action="/success" method="post" netlify-honeypot="bot-field" netlify> */}
-            <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
+            <form name="contact" method="post" action="/success/" data-netlify="true" data-netlify-honeypot="bot-field">
                 <input type="hidden" name="form-name" value="contact" />
                 <input type="hidden" name="bot-field" />
                 <div className="field half first">
