550195b63bef7efc391d459697021cce3a8c9c4c
Aldo Monteiro <aldomonteiro@gmail.com>
2018-09-27 20:28:05 -0300

    form

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
       options: {
         trackingId: "UA-126449054-1",
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
