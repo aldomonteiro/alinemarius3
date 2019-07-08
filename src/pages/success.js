55a7ffa783bc3f9495901cc7b2c3670b846aca7d
Aldo Monteiro <aldomonteiro@gmail.com>
2019-07-08 16:46:45 -0300

    fixes

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,41 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
+import Layout from '../components/Layout'
 
-import HeaderGeneric from '../components/Layout/HeaderGeneric'
-
-const Sucesso = props => {
-  return (
-    <div>
-      <Helmet title={get(props, 'data.site.siteMetadata.title')} />
-      <HeaderGeneric />
-      <div id="main">
-        <section id="content" className="main">
-          <h1><span role="img" aria-label="Sucesso!">🙌</span></h1>
-          <h2>Recebi os seus dados</h2>
-          <p>Agora, fique atenta(o) a sua caixa de entrada. Se você não
-            receber nada, dá uma olhada na caixa de Spam ou de e-mails indesejados. Você vai receber
-            as instruções para se inscrever na minha lista e fazer o download do material, tá bom?
-          </p>
-          <p>
-            <Link to="/">Voltar para a página inicial e continuar navegando</Link>
-          </p>
-        </section>
-      </div>
-
-    </div>
-  )
-}
-
-export default Sucesso
-
-export const pageQuery = graphql`
-  query SuccessQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+export default () =>
+  <Layout>
+    <section id="content" className="main">
+      <h1><span role="img" aria-label="Sucesso!">🙌</span></h1>
+      <h2>Recebi os seus dados</h2>
+      <p>Agora, fique atenta(o) a sua caixa de entrada. Se você não
+        receber nada, dá uma olhada na caixa de Spam ou de e-mails indesejados. Você vai receber
+        as instruções para fazer o download do material, tá bom?
+      </p>
+      <p>
+        <Link to="/">Voltar para a página inicial e continuar navegando</Link>
+      </p>
+    </section>
+  </Layout>
