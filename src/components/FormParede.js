30579d8a0e8b3929d75d0418173173863c137c32
Aldo <aldomonteiro@gmail.com>
2020-05-22 22:35:39 -0300

    Update FormParede.js

--- src/components/FormParede.js
+++ src/components/FormParede.js
@@ -13,29 +13,14 @@ const FormParede = (props) => (
           <input type="email" name="email" id="email" />
           <br />
         </div>
-        <input type="hidden" name="list" value="2JCA35aq763UdZbGAD3gJjTQ" />
+        <input type="hidden" name="list" value="dMrUnu892v4dEoYboaB892iKGA" />
         <input type="hidden" name="subform" value="yes" />
         <ul className="actions">
           <li>
             <input type="submit" name="submit" id="submit" value="Quero receber!" className="special" />
           </li>
         </ul>
-      </form>      {/* <form name="formCombinado" method="post" action="/success/" data-netlify="true" data-netlify-honeypot="bot-field">
-        <input type="hidden" name="form-name" value="formCombinado" />
-        <input type="hidden" name="bot-field" />
-        <div className="field half first">
-          <label htmlFor="name">Nome</label>
-          <input type="text" name="name" id="name" placeholder="Informe o seu nome" />
-        </div>
-        <div className="field half">
-          <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" placeholder="O seu melhor e-mail.." />
-          <br />
-        </div>
-        <ul className="actions">
-          <li><input type="submit" value="Quero receber!" className="special" /></li>
-        </ul>
-      </form> */}
+      </form>
     </div>
   </section>
 )
